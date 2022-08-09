5f8e1994bb0bced3c58af2beff1be54ecf62eb98
Gaurav Bhardwaj <bhardwajjgaurav@gmail.com>
2022-08-09 18:56:18 +0530

    Added contraint and functionality to the buttons
    Signed-off-by: Gaurav Bhardwaj <bhardwajjgaurav@gmail.com>

--- Musical/ViewController.swift
+++ Musical/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import AVFoundation // Audio visual Foundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+      playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName:String){
+        let url = Bundle.main.url(forResource:soundName, withExtension: "wav")
+        audioPlayer = try! AVAudioPlayer(contentsOf: url!)
+        audioPlayer?.play()
+    }
 
 }
 
